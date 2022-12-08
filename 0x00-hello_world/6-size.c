7f945d5e040541a8a100198e3f67b7568b81dcc5
aza-riah <azariahmelkamu@protonmail.com>
2022-12-07 23:36:02 -0800

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,15 @@
 
 int main(void)
 {
-char size_char;
-int size_int;
-long int size_lg_int;
-long long size_lg_lg;
-float size_float;
+char c_char;
+int c_int;
+long int c_long;
+long long c_long_long;
+float c_float;
 
-printf("size of char: %lu byte(s)\n", sizeof(size_char));
-printf("size of int: %lu byte(s)\n", sizeof(size_int));
-printf("size of long int: %lu byte(s)\n", sizeof(size_lg_int));
-printf("size of long long int: %lu byte(s)\n", sizeof(size_lg_lg));
-printf("size of float: %lu byte(s)\n", sizeof(size_float));
+printf("size of char: %lu byte(s)\n", sizeof(c_char));
+printf("size of int: %lu byte(s)\n", sizeof(c_int));
+printf("size of long int: %lu byte(s)\n", sizeof(c_long));
+printf("size of long long int: %lu byte(s)\n", sizeof(c_long_long));
+printf("size of float: %lu byte(s)\n", sizeof(c_float));
 }
