05df240cffc1172ae509f92bf0ee7421e91e1a32
aza-riah <azariahmelkamu@protonmail.com>
2022-12-08 00:38:39 -0800

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
 * main - Entry point
 *
@@ -10,6 +11,6 @@
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59, stderr);
-	return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
