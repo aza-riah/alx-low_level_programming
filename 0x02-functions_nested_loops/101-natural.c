3f2971337494c5105f0e57b320411d2c5721414d
aza-riah <azariahmelkamu@protonmail.com>
2022-12-13 09:59:56 -0800

    new fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,48 +1,21 @@
-#include "main.h"
-/**
- * print_times_table - prints the times table
- * @n: takes in an integer
- */
-void print_times_table(int n)
-{
-	int i, j, k;
-
-	if (n > 15 || n < 0)
-		return;
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			k = i * j;
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			}
-			else if (k <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			}
-			else if (k >= 10 && k < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 100) + '0');
-				_putchar(k / 10 % 10 + '0');
-				_putchar(k % 10 + '0');
-			}
-		}
-		_putchar('\n');
-	}
-}
+#include <stdio.h> 
+ /** 
+  * main - prints the sum of all the multiples of 3 or 5 below 1024 
+  * @void: void 
+  * Return: 0 Success 
+  */ 
+ int main(void) 
+ { 
+         int n; 
+         int sum = 0; 
+  
+         for (n = 0; n < 1024; n++) 
+         { 
+                 if (n % 3 == 0 || n % 5 == 0) 
+                 { 
+                         sum += n; 
+                 } 
+         } 
+         printf("%d\n", sum); 
+         return (0); 
+ }
