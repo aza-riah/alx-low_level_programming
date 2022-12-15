a4bbfa55df7d375ee0fbb721c92c43794c908cfe
aza-riah <azariahmelkamu@protonmail.com>
2022-12-15 02:56:26 -0800

    fix 12

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,26 +1,33 @@
 #include "main.h"
+
 /**
- * print_square - prints square
+ * print_triangle - prints triangle
  *
- * @size: integer to set square size
+ * @size: size of triangle
  */
-void print_square(int size)
+void print_triangle(int size)
 {
 	int i;
 	int j;
+	int k;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
 	} else
 	{
-		for (i = 0; i < size; i++)
+
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < size; j++)
+			for (j = size - i ; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i; k++)
 			{
 				_putchar('#');
 			}
 			_putchar('\n');
 		}
 	}
-}
+}	
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,15 @@
 #include "main.h"
 /**
- * more_numbers - prints the numbers
+ * print_numbers - prints the numbers
  *
  */
-void more_numbers(void)
+void print_numbers(void)
 {
 	int i;
-	int h;
 
-	for (h = 0; h <= 9; h++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (i = 0; i <= 14; i++)
-		{
-			if (i > 9)
-			{
-				_putchar(i / 10 + '0');
-			}
-			_putchar(i % 10 + '0');
-		}
-		_putchar('\n');
+		_putchar(i);
 	}
+	_putchar('\n');
 }
